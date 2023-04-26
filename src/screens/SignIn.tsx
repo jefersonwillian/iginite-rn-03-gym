5f83cc79e9e0eef97d26e610202dc1df4d597346
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-26 20:18:36 -0300

    Component Library - Finalizando os Inputs

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -6,7 +6,7 @@ import { Input } from "@components/Input";
 
 export function SignIn() {
     return (
-        <VStack flex={1} bg="gray.700">
+        <VStack flex={1} bg="gray.700" px={10}>
             <Image
                 source={BackgroundImg}
                 alt="Pessoas treinando"
@@ -28,8 +28,16 @@ export function SignIn() {
                 </Heading>
             </Center>
 
-            <Input placeholder="E-mail" />
-            <Input placeholder="Senha" type="password"/>
+            <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+
+            />
+            <Input
+                placeholder="Senha"
+                secureTextEntry
+            />
         </VStack>
     );
 }
