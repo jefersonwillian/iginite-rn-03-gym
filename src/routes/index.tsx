574514c1e811fc4ebd9d73f03da1a4c404495735
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-27 19:54:51 -0300

    Rotas Públicas - Modificando o tema da navegação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,20 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { useTheme, Box } from 'native-base';
 
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
+    const { colors } = useTheme();
+
+    const theme = DefaultTheme;
+    theme.colors.background = colors.gray[700];
+    
     return (
-        <NavigationContainer>
-            <AuthRoutes />
-        </NavigationContainer>
+        <Box flex={1} bg="gray.700">
+            <NavigationContainer theme={theme}>
+                <AuthRoutes />
+            </NavigationContainer>
+        </Box>
+        
     );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -10,7 +10,7 @@ export function SignIn() {
         <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
 
 
-            <VStack flex={1} bg="gray.700" px={10} pb={16}>
+            <VStack flex={1} px={10} pb={16}>
                 <Image
                     source={BackgroundImg}
                     alt="Pessoas treinando"
