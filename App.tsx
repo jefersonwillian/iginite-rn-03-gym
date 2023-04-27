79e9fbd38ba45249d98d86deae7bb0656485c39a
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-27 19:49:14 -0300

    Rotas Públicas - Criando o contexto de navegação

--- App.tsx
+++ App.tsx
@@ -8,8 +8,8 @@ import { Center, NativeBaseProvider } from "native-base";
 import { StatusBar } from "react-native";
 
 import { THEME } from "./src/theme";
-import { SignIn } from "@screens/SignIn";
-import { SignUp } from "@screens/SignUp";
+
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -23,7 +23,7 @@ export default function App() {
       />
 
       {fontsLoaded ? (
-        <SignUp />
+        <Routes />
       ) : (
         <Loading />
       )}
