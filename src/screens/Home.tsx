c72e14911bf0984d6e4474e8f22d2ef4ce76c74f
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-27 21:45:36 -0300

    Finalizando as Interfaces - Exibindo o grupo selecionado

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,25 @@
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 import { HStack, VStack } from 'native-base';
+import { useState } from 'react';
 
 export function Home() {
+    const [groupSelected, setGroupSelected] = useState('costa');
     return (
         <VStack flex={1}>
             <HomeHeader />
 
             <HStack>
-                <Group name="Costa" />
-                <Group name="ombro" />
+                <Group
+                    name="costa"
+                    isActive={groupSelected === 'costa'}
+                    onPress={() => setGroupSelected('costa')}
+                />
+                <Group
+                    name="ombro"
+                    isActive={groupSelected === 'ombro'}
+                    onPress={() => setGroupSelected('ombro')}
+                />
             </HStack>
         </VStack>
     );
