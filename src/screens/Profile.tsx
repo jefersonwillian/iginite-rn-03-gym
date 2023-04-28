34de5db46d4dcd22d4053467b6bbc049c2a0b64e
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-28 20:13:29 -0300

    Finalizando as Interfaces - Efeito Skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,37 @@
-import { Center, Text } from 'native-base';
+import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
+import { Center, ScrollView, Skeleton, Text, VStack } from 'native-base';
+import { useState } from 'react';
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+    const [photoIsLoading, setPhotoIsLoading] = useState(true);
+    
     return (
-        <Center flex={1}>
-            <Text color="white">Profile</Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title='Perfil' />
+
+            <ScrollView>
+                <Center mt={6} px={10}>
+                    {
+                        photoIsLoading ?
+                            <Skeleton
+                                w={PHOTO_SIZE}
+                                h={PHOTO_SIZE}
+                                rounded="full"
+                                startColor="gray.500"
+                                endColor="gray.400"
+                            />
+                            :
+                            <UserPhoto
+                                source={{ uri: 'https://github.com/jefersonwillian.png' }}
+                                alt="Foto do usuário"
+                                size={PHOTO_SIZE}
+                            />
+                    }
+                </Center>
+            </ScrollView>
+        </VStack>
     );
 }
