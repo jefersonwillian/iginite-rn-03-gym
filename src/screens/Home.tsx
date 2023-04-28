17cff5ce529a50cbdca60ab237d8d477ab908994
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-28 20:34:13 -0300

    Finalizando as Interfaces - Navegando para tela de exercício

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,8 @@
 import { ExerciseCard } from '@components/ExerciseCard';
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
+import { useNavigation } from '@react-navigation/native';
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
 import { FlatList, HStack, Heading, VStack, Text } from 'native-base';
 import { useState } from 'react';
 
@@ -9,6 +11,13 @@ export function Home() {
     const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro'])
     const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terras']);
     const [groupSelected, setGroupSelected] = useState('Costas');
+
+    const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+    function handleOpenExerciseDetails() {
+        navigation.navigate('exercise');
+    }
+    
     return (
         <VStack flex={1}>
             <HomeHeader />
@@ -47,7 +56,7 @@ export function Home() {
                     data={exercises}
                     keyExtractor={item => item}
                     renderItem={({ item }) => (
-                        <ExerciseCard />
+                        <ExerciseCard onPress={handleOpenExerciseDetails} />
                     )}
                     showsVerticalScrollIndicator={false}
                     _contentContainerStyle={{
