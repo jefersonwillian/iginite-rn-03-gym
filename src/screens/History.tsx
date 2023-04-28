55cdaa1a8c49cc090371544b58d856d27772b264
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-28 19:54:21 -0300

    Finalizando as Interfaces - Mensagem de lista vazia

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,6 +1,6 @@
 import { HistoryCard } from '@components/HistoryCard';
 import { ScreenHeader } from '@components/ScreenHeader';
-import { Heading, SectionList, VStack } from 'native-base';
+import { Heading, SectionList, VStack, Text } from 'native-base';
 import { useState } from 'react';
 
 export function History() {
@@ -31,6 +31,13 @@ export function History() {
                     </Heading>
                 )}
                 px={8}
+                contentContainerStyle={!exercises.length && { flex: 1, justifyContent: 'center' }}
+                ListEmptyComponent={() => (
+                    <Text color="gray.100" textAlign="center">
+                        Não há exercícios registrados ainda. {'\n'}
+                        Vamos fazer exercícios hoje?
+                    </Text>
+                )}
             />
         </VStack>
     );
