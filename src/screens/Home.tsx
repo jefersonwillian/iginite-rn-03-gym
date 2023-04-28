66b1e03eef9175641e82ca8d38a3662cf17cce7f
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-28 19:34:41 -0300

    Finalizando as Interfaces - Recap da Home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -19,7 +19,7 @@ export function Home() {
                 renderItem={({ item }) => (
                     <Group
                         name={item}
-                        isActive={groupSelected === item}
+                        isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
                         onPress={() => setGroupSelected(item)}
                     />
                 )}
