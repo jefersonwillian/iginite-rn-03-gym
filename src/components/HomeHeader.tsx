da79ef34d40821df8e429334873501a3a25650f6
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-27 21:30:55 -0300

    Finalizando as Interfaces - Componente UserPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,15 +1,22 @@
 import { Heading, HStack, Text, VStack } from 'native-base';
+import { UserPhoto } from './UserPhoto';
 
 export function HomeHeader() {
     return (
         <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+            <UserPhoto
+                source={{ uri: 'https://github.com/jefersonwillian.png' }}
+                size={16}
+                alt="Imagem do usuário"
+                mr={4}
+            />
             <VStack>
                 <Text color="gray.100" fontSize="md">
                     Olá,
                 </Text>
 
                 <Heading color="gray.100" fontSize="md">
-                    Rodrigo
+                    Jeferson Carvalho
                 </Heading>
             </VStack>
         </HStack>
