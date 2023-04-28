ec7d27501099833cefb324cb6d09909d69bb32a3
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-27 21:33:32 -0300

    Finalizando as Interfaces - Botão Logout

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,5 +1,7 @@
-import { Heading, HStack, Text, VStack } from 'native-base';
+import { Heading, HStack, Icon, Text, VStack } from 'native-base';
 import { UserPhoto } from './UserPhoto';
+import { TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export function HomeHeader() {
     return (
@@ -19,6 +21,14 @@ export function HomeHeader() {
                     Jeferson Carvalho
                 </Heading>
             </VStack>
+            <TouchableOpacity>
+                <Icon
+                    as={MaterialIcons}
+                    name="logout"
+                    color="gray.200"
+                    size={7}
+                />
+            </TouchableOpacity>
         </HStack>
     );
 }
