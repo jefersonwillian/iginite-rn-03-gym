f492de03d3c0a8783d1617824f2ad2ccab77893b
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-28 20:31:07 -0300

    Finalizando as Interfaces - Recap do Profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -15,7 +15,7 @@ export function Profile() {
         <VStack flex={1}>
             <ScreenHeader title='Perfil' />
 
-            <ScrollView>
+            <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
                 <Center mt={6} px={10}>
                     {
                         photoIsLoading ?
@@ -53,7 +53,7 @@ export function Profile() {
                     />
                 </Center>
                 <VStack px={10} mt={12} mb={9}>
-                    <Heading color="gray.200" fontSize="md" mb={2}>
+                    <Heading color="gray.200" fontSize="md" mb={2} alignSelf="flex-start" mt={12}>
                         Alterar senha
                     </Heading>
 
