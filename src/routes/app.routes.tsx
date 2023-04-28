1227d6bbe23f8fb37eb4227eee6c41da38ff7555
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-27 21:03:19 -0300

    Rotas Privadas - Alterando os ícones da Bottom Navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,9 +1,14 @@
 import { BottomTabNavigationProp, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import HomeSvg from '@assets/home.svg';
+import HistorySvg from '@assets/history.svg';
+import ProfileSvg from '@assets/profile.svg';
+
 import { Home } from '@screens/Home';
 import { Exercise } from '@screens/Exercise';
 import { History } from '@screens/History';
 import { Profile } from '@screens/Profile';
+import { useTheme } from 'native-base';
 
 type AppRoutes = {
     home: undefined;
@@ -17,21 +22,40 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
+    const { sizes } = useTheme();
+
+    const iconSize = sizes[6];
+    
     return (
         <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
             <Screen
                 name='home'
                 component={Home}
+                options={{
+                    tabBarIcon: ({ color, }) => (
+                        <HomeSvg fill={color} width={iconSize} height={iconSize} />
+                    )
+                }}
             />
 
             <Screen
                 name='history'
                 component={History}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <HistorySvg fill={color} width={iconSize} height={iconSize} />
+                    )
+                }}
             />
 
             <Screen
                 name='profile'
                 component={Profile}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <ProfileSvg fill={color} width={iconSize} height={iconSize} />
+                    )
+                }}
             />
 
             <Screen
