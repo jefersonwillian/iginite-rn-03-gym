dae61a93351bde49f97cbf0bdd7c77765dd359b6
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-27 21:53:16 -0300

    Finalizando as Interfaces - Header da lista de exercícios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,26 +1,45 @@
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import { HStack, VStack } from 'native-base';
+import { FlatList, HStack, Heading, VStack, Text } from 'native-base';
 import { useState } from 'react';
 
 export function Home() {
     const [groupSelected, setGroupSelected] = useState('costa');
+    const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro'])
     return (
         <VStack flex={1}>
             <HomeHeader />
 
-            <HStack>
-                <Group
-                    name="costa"
-                    isActive={groupSelected === 'costa'}
-                    onPress={() => setGroupSelected('costa')}
-                />
-                <Group
-                    name="ombro"
-                    isActive={groupSelected === 'ombro'}
-                    onPress={() => setGroupSelected('ombro')}
-                />
-            </HStack>
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Group
+                        name={item}
+                        isActive={groupSelected === item}
+                        onPress={() => setGroupSelected(item)}
+                    />
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                _contentContainerStyle={{
+                    px: 8,
+                }}
+                my={10}
+                maxH={10}
+            />
+
+            <VStack px={8}>
+                <HStack justifyContent="space-between" mb={5}>
+                    <Heading color="gray.200" fontSize="md">
+                        Exercícios
+                    </Heading>
+
+                    <Text color="gray.200" fontSize="sm">
+                        4
+                    </Text>
+                </HStack>
+            </VStack>
         </VStack>
     );
 }
