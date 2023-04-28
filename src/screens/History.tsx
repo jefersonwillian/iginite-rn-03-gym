2e7fb51b1054f462f2b5e19e62f38aea8dc854d1
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-28 19:43:24 -0300

    Finalizando as Interfaces - Componente HistoryCard

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,3 +1,4 @@
+import { HistoryCard } from '@components/HistoryCard';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { Center, Text, VStack } from 'native-base';
 
@@ -5,6 +6,10 @@ export function History() {
     return (
         <VStack flex={1}>
             <ScreenHeader title='Histórico' />
+
+            <HistoryCard />
+            <HistoryCard />
+            <HistoryCard />
         </VStack>
     );
 }
