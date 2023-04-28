020003ded6ab996eb424590b710bd43e33b37459
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-04-27 21:09:04 -0300

    Rotas Privadas - Estilizando menu ativo e inativos

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -22,12 +22,17 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
-    const { sizes } = useTheme();
+    const { sizes, colors } = useTheme();
 
-    const iconSize = sizes[6];
-    
+    const iconSize = sizes[7];
+
     return (
-        <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+        <Navigator screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: colors.green[500],
+            tabBarInactiveTintColor: colors.gray[200],            
+        }}>
             <Screen
                 name='home'
                 component={Home}
