1bd596c7b3392810ead4802f0921a21ae57c7f22
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-05-01 23:08:21 -0300

    toast

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from 'native-base';
+import { Alert, TouchableOpacity } from 'react-native';
+import { Center, Heading, ScrollView, Skeleton, Text, VStack, useToast } from 'native-base';
 
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
@@ -9,12 +9,15 @@ import { UserPhoto } from '@components/UserPhoto';
 
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
+import { FileInfo } from 'expo-file-system';
 
 const PHOTO_SIZE = 33;
 
 export function Profile() {
     const [photoIsLoading, setPhotoIsLoading] = useState(false);
     const [userPhoto, setUserPhoto] = useState('https://github.com/jefersonwillian.png');
+
+    const toast = useToast();
 
     async function handleUserPhotoSelected() {
         setPhotoIsLoading(true);
@@ -31,8 +34,18 @@ export function Profile() {
             }
 
             if (photoSelected.assets.length) {
-                const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
-                console.log(photoInfo);
+                const photoInfo: any = await FileSystem.getInfoAsync(photoSelected.assets[0].uri, { size: true, md5: true })
+                
+             
+                
+                if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+                    return toast.show({
+                        title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+                        placement: 'top',
+                        bgColor: 'red.500',
+                        duration: 5000
+                    })
+                }
 
                 setUserPhoto(photoSelected.assets[0].uri);
             }
@@ -63,6 +76,7 @@ export function Profile() {
                                 source={{ uri: userPhoto }}
                                 alt="Foto do usuário"
                                 size={PHOTO_SIZE}
+                                resizeMode='cover'
                             />
                     }
                     <TouchableOpacity onPress={handleUserPhotoSelected}>
