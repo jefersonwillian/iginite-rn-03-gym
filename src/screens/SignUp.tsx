f4288d17585b45058a381055b7c9de7b5d287fff
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-05-02 18:40:48 -0300

    Formulários - Tipando os dados do formulário

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -8,9 +8,16 @@ import BackgroundImg from '@assets/background.png';
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
+type FormDataProps = {
+    name: string;
+    email: string;
+    password: string;
+    password_confirm: string;
+}
+
 export function SignUp() {
 
-    const { control, handleSubmit } = useForm();
+    const { control, handleSubmit } = useForm<FormDataProps>();
 
     const navigation = useNavigation();
 
