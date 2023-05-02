05d234acf78d55180374695c9af151a19a1eeac6
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-05-01 22:38:24 -0300

    Galeria de Fotos - Buscando infomações do arquivo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Center, Heading, ScrollView, Skeleton, Text, VStack } from 'native-base';
+
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from 'native-base';
-import { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
+
 import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 const PHOTO_SIZE = 33;
 
@@ -28,6 +31,9 @@ export function Profile() {
             }
 
             if (photoSelected.assets.length) {
+                const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
+                console.log(photoInfo);
+
                 setUserPhoto(photoSelected.assets[0].uri);
             }
         } catch (error) {
