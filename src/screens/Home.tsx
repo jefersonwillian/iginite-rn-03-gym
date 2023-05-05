ad6b81be6be4e850a1766551cb0d01fb437e9ef5
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-05-05 17:11:56 -0300

    Consumindo o Back-end - Buscando e Listando Grupos

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,20 +3,45 @@ import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 import { useNavigation } from '@react-navigation/native';
 import { AppNavigatorRoutesProps } from '@routes/app.routes';
-import { FlatList, HStack, Heading, VStack, Text } from 'native-base';
-import { useState } from 'react';
+import { api } from '@services/api';
+import { AppError } from '@utils/AppError';
+import { FlatList, HStack, Heading, VStack, Text, useToast } from 'native-base';
+import { useEffect, useState } from 'react';
 
 export function Home() {
 
-    const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro'])
+    const [groups, setGroups] = useState<string[]>([]);
     const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terras']);
-    const [groupSelected, setGroupSelected] = useState('Costas');
+    const [groupSelected, setGroupSelected] = useState('');
 
     const navigation = useNavigation<AppNavigatorRoutesProps>();
 
+    const toast = useToast();
+    
     function handleOpenExerciseDetails() {
         navigation.navigate('exercise');
     }
+    
+    async function fetchGroups() {
+        try {
+            const response = await api.get('/groups');
+            setGroups(response.data);
+
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+            const title = isAppError ? error.message : 'Não foi possível carregar os grupos musculares';
+
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        }
+    }
+
+    useEffect(() => {
+        fetchGroups();
+    }, [])
     
     return (
         <VStack flex={1}>
