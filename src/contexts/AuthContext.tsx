dba15ea0d3fe7ed68f44b814565e10009b48c9b6
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-05-05 16:01:56 -0300

    Autenticação JWT - Anexando token nas requisições

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
+import { storageAuthTokenSave } from "@storage/storageAuthToken";
 import { storageUserGet, storageUserRemove, storageUserSave } from "@storage/storageUser";
 import { ReactNode, createContext, useEffect, useState } from "react";
 
@@ -21,13 +22,31 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     const [user, setUser] = useState<UserDTO>({} as UserDTO);
     const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true); 
 
+    async function storageUserAndToken(userData: UserDTO, token: string) {
+
+        try {
+            setIsLoadingUserStorageData(true);
+
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+            await storageUserSave(userData);
+            await storageAuthTokenSave(token);
+            setUser(userData);
+        } catch (error) {
+            throw error
+        } finally {
+            setIsLoadingUserStorageData(false);
+        }
+
+    }
+    
     async function singIn(email: string, password: string) {
         try {
             const { data } = await api.post('/sessions', { email, password });
+            console.log("🚀 ~ file: AuthContext.tsx:46 ~ singIn ~ data:", data)
 
-            if (data.user) {
-                setUser(data.user);
-                storageUserSave(data.user)
+            if (data.user && data.token) {
+                storageUserAndToken(data.user, data.token);
             }
         } catch (error) {
             throw error
