74274fca9e8ddcfd4e93a109798ee96f46a33982
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-05-06 16:32:21 -0300

    Consumindo o Back-end - Listando o histórico

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,29 +1,54 @@
 import { HistoryCard } from '@components/HistoryCard';
 import { ScreenHeader } from '@components/ScreenHeader';
-import { Heading, SectionList, VStack, Text } from 'native-base';
-import { useState } from 'react';
+import { HistoryByDayDTO } from '@dtos/HistoryByDayDTO';
+import { useFocusEffect } from '@react-navigation/native';
+import { api } from '@services/api';
+import { AppError } from '@utils/AppError';
+import { Heading, SectionList, VStack, Text, useToast } from 'native-base';
+import { useCallback, useState } from 'react';
 
 export function History() {
-    const [exercises, setExercises] = useState([
-        {
-            title: '26.08.22',
-            data: ["Puxada frontal", "Remada unilateral"]
-        },
-        {
-            title: '27.08.22',
-            data: ["Puxada frontal"]
+    const [isLoading, setIsLoading] = useState(true);
+
+    const [exercises, setExercises] = useState<HistoryByDayDTO[]>([]);
+
+    const toast = useToast();
+
+    async function fetchHistory() {
+        try {
+            setIsLoading(true);
+            const response = await api.get('/history');
+            setExercises(response.data);
+
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+            const title = isAppError ? error.message : 'Não foi possível carregar os detalhes do exercício';
+
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            });
+        } finally {
+            setIsLoading(false);
         }
-    ]);
-    
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            fetchHistory()
+        }, [])
+    )
+
     return (
         <VStack flex={1}>
             <ScreenHeader title='Histórico' />
 
             <SectionList
                 sections={exercises}
-                keyExtractor={item => item}
+                keyExtractor={item => item.id}
                 renderItem={({ item }) => (
-                    <HistoryCard />
+                    <HistoryCard data={item} />
                 )}
                 renderSectionHeader={({ section }) => (
                     <Heading color="gray.200" fontSize="md" mt={10} mb={3} fontFamily="heading">
